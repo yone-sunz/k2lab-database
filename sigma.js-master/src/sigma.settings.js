dcc496be219fe7972c2d09d150062d774ec3466c
Kai <kai-say-gogo@outlook.jp>
2021-09-15 21:04:50 +0900

    スマホ判別の関数だけ追加

--- sigma.js-master/src/sigma.settings.js
+++ sigma.js-master/src/sigma.settings.js
@@ -7,6 +7,15 @@
   // Packages initialization:
   sigma.utils.pkg('sigma.settings');
 
+  function isSmartPhone() {
+    if (window.matchMedia && window.matchMedia('(max-device-width: 640px)').matches) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
   var settings = {
     /**
      * GRAPH SETTINGS:
